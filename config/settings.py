5a69c8835ceac825d0f1fcca8ad6a5e5ab5ca6a5
karina alfonso <karinaalfonso22@gmail.com>
2026-05-24 22:10:01 -0500

    Nuevo dominio agregado a cors

--- config/settings.py
+++ config/settings.py
@@ -126,7 +126,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    "https://frontend-caudal.vercel.app",
+    "https://caudal-uis-project.vercel.app/",
 ]
 
 REST_FRAMEWORK = {
